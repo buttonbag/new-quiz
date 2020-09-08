782f8c73e7806f2f97dbb6c8486051d3634dcbf7
Louie Camacho <louiecamacho@Elizabeths-MacBook-Air.local>
2020-09-07 22:42:09 -0500

    choice dataset logging okay

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
 /* load questions and answers from questions.json file into html dom */
+const question = document.querySelector('#question');
+const choices = document.querySelectorAll('.choice');
+
+let questionCounter = 0;
+let currentQuestion = {};
+let availableQuestions = [];
+
+fetch("questions.json")
+.then(res => res.json())
+.then(data=> {
+    console.log(data);
+});
+
+askNewQuestion = () => {
+    // questionCounter++;
+}
+
+choices.forEach( choice => {
+    choice.addEventListener("click", e => console.log(e.target.dataset["number"]));
+});
